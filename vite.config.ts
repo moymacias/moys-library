4b52e1660c3cec8e5f2616ad623d39099173b4a5
Moy Macías <moymacias@outlook.com>
2024-02-13 00:17:06 -0600

    ci: :wrench: Liter and prettier rules added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'pepsico-ds',
+      fileName: 'pepsico-ds',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'React-dom',
+        },
+      },
+    },
+  },
 })
